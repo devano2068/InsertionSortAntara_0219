ce53b19102ad7fcf41b525ab5bd3ac7479714c28
devano2068 <devano27921@gmail.com>
2026-07-16 10:36:43 +0700

    secc void

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -21,32 +21,19 @@ void input() // membuat prosedur input
         }
     }
 
+    cout << endl;
+    cout << "=========================" << endl; // membuat jarak per baris program
+    cout << " Masukkan Element Array " << endl;  // membuat tampilan susunan data element array
+    cout << "=========================" << endl;
 
-
-void insertionSort()
-{ // membuat prosedur insertionsort
-    int temp, i, j; // membuat variabel temp untuk menyimpan data sementara dan variabel i dan j sebagai penanda
-
-    for (i = 1; i <= n - 1; i++) // 1. looping dengan i dimulai dari 1 hinggga n-1
+    for (int i = 0; i < n; i++) // membuat perulangan untuk memasukkan input array
     {
-        temp = arr[i]; // 2. menyimpan nilai arr[i] ke variabel sementara temp
-        j = i - 1;     // 3. set nilai j = i - 1
-        while (j >= 0 && arr[j] > temp)
-        {
-            // looping while apa bila j lebih besar atau sama dengan nol dan nilai arr[j] lebih besar dari variabel temp
-            arr[j + 1] = arr[j]; // 4a. geser isi arr[j] ke arr[j+1]
-            j--;                 // 4b. decrement nilai j
-        }
-
-        arr[j + 1] = temp; // 5. letakkan temp di arr[j+1] jika while tidak memenuhi syarat
-
-        cout << "\nPass " << i << " : "; // untuk mendisplay pass dari algoritma
-        for (int k = 0; k < n; k++)      // looping untuk mendisplay isi pass algoritma
-        {
-            cout << arr[k] << " "; // untuk mendisplay angka pada pass ke k
-        }
+        cout << "Masukkan data ke " << i + 1 << " : "; // memasukkan atau menginputkan nilai data n
+        cin >> arr[i];                                  // menyimpan nilai data n ke arr index ke - i
     }
 }
+
+
 
 void output() // prosedur output
 {
