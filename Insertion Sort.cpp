792ace17e668da939e9cd275763517de7a0f2615
devano2068 <devano27921@gmail.com>
2026-07-16 10:33:46 +0700

    first commit

---
+++ Insertion Sort.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // membuat array dengan panjang data 20
+int n;       // membuat variabel n untuk input panjang array
+
+void input() // membuat prosedur input
+{
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data Pada Array : "; // membuat inputan jumlah element array
+        cin >> n;                                     // memanggil variabel n untuk menerima input
+
+        if (n <= 20) // membuat kondisi n kurang dari atau sama dengan 20
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nPanjang Data Maksimal 20" << endl; // menampilkan kondisi jika panjang data lebih dari 20
+        }
+    }
+
+    cout << endl;
+    cout << "=========================" << endl; // membuat jarak per baris program
+    cout << " Masukkan Element Array " << endl;  // membuat tampilan susunan data element array
+    cout << "=========================" << endl;
+
+    for (int i = 0; i < n; i++) // membuat perulangan untuk memasukkan input array
+    {
+        cout << "Masukkan data ke " << i + 1 << " : "; // memasukkan atau menginputkan nilai data n
+        cin >> arr[i];                                  // menyimpan nilai data n ke arr index ke - i
+    }
+}
+
+void insertionSort()
+{ // membuat prosedur insertionsort
+    int temp, i, j; // membuat variabel temp untuk menyimpan data sementara dan variabel i dan j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++) // 1. looping dengan i dimulai dari 1 hinggga n-1
+    {
+        temp = arr[i]; // 2. menyimpan nilai arr[i] ke variabel sementara temp
+        j = i - 1;     // 3. set nilai j = i - 1
+        while (j >= 0 && arr[j] > temp)
+        {
+            // looping while apa bila j lebih besar atau sama dengan nol dan nilai arr[j] lebih besar dari variabel temp
+            arr[j + 1] = arr[j]; // 4a. geser isi arr[j] ke arr[j+1]
+            j--;                 // 4b. decrement nilai j
+        }
+
+        arr[j + 1] = temp; // 5. letakkan temp di arr[j+1] jika while tidak memenuhi syarat
+
+        cout << "\nPass " << i << " : "; // untuk mendisplay pass dari algoritma
+        for (int k = 0; k < n; k++)      // looping untuk mendisplay isi pass algoritma
+        {
+            cout << arr[k] << " "; // untuk mendisplay angka pada pass ke k
+        }
+    }
+}
+
+void output() // prosedur output
+{
+    cout << endl;
+    cout << "===============================" << endl; // memberi baris baru
+    cout << " Element Yang Sudah Di Urut " << endl;   // tampilan elemnt yang sudah di urut
+    cout << "===============================" << endl;
+
+    for (int i = 0; i < n; i++) // looping untuk mendisplay isi arr yang sudah di urut
+    {
+        cout << "Data ke " << i + 1 << " : " << arr[i] << endl; // untuk mendisplay isi arr yang sudah di urut
+    }
+}
+
+int main()
+{
+    input();         // memanggil prosedur input()
+    insertionSort(); // memanggil prosedur insertionSort()
+    output();        // memanggil prosedur output()
+}
---
+++ Shell Sort.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int n;      // Menyimpan jumlah elemen aktif yang diinputkan
+int arr[20]; // Array statis langsung dipesan dengan kapasitas maksimal 20 elemen
+
+void inputData(int arr[], int &r)
+{
+    cout << "==========================================" << endl;
+    cout << "=     PELAJARAN ALGORITMA SHELL SORT     =" << endl;
+    cout << "=                UKY 1E8                 =" << endl;
+    cout << "==========================================" << endl;
+    cout << endl;
+
+    cout << "Masukkan panjang array: ";
+    cin >> n;
+
+    if (r > 20)
+    {
+        cout << "elemen tidak boleh melebihi 20" << endl;
+        exit(0);
+    }
+
+    cout << "Masukkan " << n << " Data yang akan diurutkan :\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke " << i + 1 << " : ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+void outputdata(int arr[], int r)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "[" << " " << arr[i] << " ]";
+    }
+    cout << endl;
+}
+
+void shellSort(int arr[], int n)
+{
+    // membuat fungsi shellSort dengan parameter array arr dan ukuran n
+    int t; // membuat variabel t untuk menyimpan data sementara saat proses pertukaran (swap)
+
+    for (int i = n / 2; i > 0; i = i / 2)
+    { // 1. looping luar untuk menentukan jarak gap, dimulai dari n/2 lalu dibagi 2 disetiap langkahnya hingga gap = 0
+        for (int j = i; j < n; j++)
+        { // 2. looping untuk menyisir elemen array dimulai dari indeks gap (i) hingga akhir array (n 1)
+            for (int k = j - i; k >= 0; k = k - i)
+            { // 3. looping dalam untuk membandingkan elemen yang berjarak sejauh gap (i) ke arah kiri
+                if (arr[k + i] >= arr[k])
+                {
+                    // 4. pengecekan jika elemen kanan (arr[k+i]) sudah lebih besar atau sama dengan elemen kiri (arr[k])
+                    break; // 4a. jika benar sudah berurutan, hentikan looping k (break)
+                }
+                else
+                {
+                    // 5. jika elemen kanan lebih kecil, maka lakukan pertukaran posisi (swap)
+                    t = arr[k];          // 5a. simpan elemen kiri ke variabel penampung sementara t
+                    arr[k] = arr[k + i]; // 5b. pindahkan nilai elemen kanan ke posisi elemen kiri
+                    arr[k + i] = t;      // 5c. pindahkan nilai sementara t ke posisi elemen kanan
+                }
+            }
+        }
+        cout << "Pengulangan Shell Sort (Gap " << i << ") : "; // untuk menampilkan informasi langkah pengulangan berdasarkan nilai gap aktif (i)
+        outputdata(arr, n);                                   // memanggil prosedur tampil-arr/array untuk mendisplay hasil susunan angka pada gap ke-i
+    }
+}
+
+int main()
+{
+    inputData(arr, n);
+
+    cout << "Data awal : " << endl;
+    outputdata(arr, n);
+    cout << endl;
+
+    cout << "----- PROSES PENGURUTAN -----" << endl;
+    shellSort(arr, n);
+    cout << "-----------------------------" << endl;
+    cout << endl;
+
+    cout << "Data yang telah diurutkan dengan algoritma Shell Sort :" << endl;
+    outputdata(arr, n);
+    cout << endl;
+
+    return 0;
+}
